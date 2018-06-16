dad35676fba07895d7484a39c60ba6c61b394b3b
Jorge Almela <jam2590@gmail.com>
2018-06-16 21:17:50 +0200

    Initial commit: Project structure

---
+++ main.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  routes = require('./api/routes/route'),
+  configuration = require('./configuration/configuration');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', routes)
+
+app.listen(port);
+console.log('Server listening on: ' + port);
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
